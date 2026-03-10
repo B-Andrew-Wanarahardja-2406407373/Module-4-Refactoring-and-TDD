62b54afc4d9eb88f55620ca1d79a8c00bf19045b
Andrew Wanarahardja <andrew.wanarahardja@gmail.com>
2026-03-10 13:29:03 +0700

    [GREEN] implement PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -4,30 +4,43 @@ import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private List<Payment> payments;
+    private List<Payment> payments = new ArrayList<>();
 
     public Payment addPayment(Payment payment) {
-        return null;
+        payments.add(payment);
+        return payment;
     }
 
     public Payment setStatus(Payment payment, String status) {
+        for (Payment repoPayment : payments) {
+            if (repoPayment.getId().equals(payment.getId())) {
+                repoPayment.setStatus(status);
+                return repoPayment;
+            }
+        }
         return null;
     }
 
     public Payment getPayment(String paymentId) {
+        for (Payment payment : payments) {
+            if (payment.getId().equals(paymentId)) {
+                return payment;
+            }
+        }
         return null;
     }
 
     public List<Payment> getAllPayments() {
-        return null;
+        return this.payments;
     }
 
     public int getSize() {
-        return 0;
+        return this.payments.size();
     }
 }
